7241f3d25d28058b59d21eac8c906f74fd644058
Hyhy3008 <90853521+Hyhy3008@users.noreply.github.com>
2026-02-22 15:11:27 +0700

    Add files via upload

---
+++ livekit-web/main.py
@@ -0,0 +1,32 @@
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from livekit import api
+
+LIVEKIT_URL = os.environ["wss://nhat-1q4pr3ug.livekit.cloud"]              # wss://xxx.livekit.cloud
+LIVEKIT_API_KEY = os.environ["APIQfwx8PQ7Dw3s"]
+LIVEKIT_API_SECRET = os.environ["9DfjEAZHApIfkL2bnhw9OisophZikbKonaVvbXkaA9x"]
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    # render HTML viewer
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "livekit_url": LIVEKIT_URL}
+    )
+
+
+@app.get("/token")
+async def token(identity: str, room: str):
+    # server-side mint token (secret stays on server)
+    at = api.AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET)\
+        .with_identity(identity)\
+        .with_name(identity)
+
+    at.with_grants(api.VideoGrants(room_join=True, room=room))
+    return {"token": at.to_jwt(), "url": LIVEKIT_URL}
